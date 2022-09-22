70c0cf6d8ace8648fa18964941bcc5f6ccaa190e
berkyd <berky.daniel@petrik.hu>
2022-09-22 09:41:36 +0200

    valami

--- src/Festmeny.java
+++ src/Festmeny.java
@@ -7,6 +7,51 @@ public class Festmeny {
         this.stilus = stilus;
     }
 
+    public Festmeny(String sor) {
+    }
+
+    public String getFesto() {
+        return festo;
+    }
+
+    public String getStilus() {
+        return stilus;
+    }
+
+    public int getLicitekSzama() {
+        return licitekSzama;
+    }
+
+    public int getLegmagasabbLicit() {
+        return legmagasabbLicit;
+    }
+
+    public LocalDateTime getLegutolsoLicitIdeje() {
+        return legutolsoLicitIdeje;
+    }
+
+    public boolean isElkelt() {
+        return elkelt;
+    }
+
+    public void setElkelt(boolean elkelt) {
+        this.elkelt = elkelt;
+    }
+    public void licit(int mertek){
+
+    };
+    public void licit(){
+        int ossz = 100;
+        if(licitekSzama<0){
+            licitekSzama++;
+            legutolsoLicitIdeje=LocalDateTime.now();
+        }
+        if(licitekSzama>0){
+            ossz=(int)(legmagasabbLicit*1.10);
+            licitekSzama++;
+            legutolsoLicitIdeje=LocalDateTime.now();
+        }
+    }
     String cim;
     String festo;
     String stilus;
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        public static void main(String[] args) throws IOException {
+            var sorok = Files.readAllLines(Paths.get("festmenyek.csv"));
+            var festmenyek=new LinkedList<Festmeny>();
+            for (var sor: sorok) {
+                var painting = new Festmeny(sor);
+                festmenyek.add(painting);
+            }
+            System.out.println(festmenyek);
+        }
     }
-}
